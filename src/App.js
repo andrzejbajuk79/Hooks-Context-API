44f651c8a5b3d9a1fe28cc4964d0f3c7df3f7b4b
andrzejbajuk79 <ptasiek3003@gmail.com>
2020-06-10 19:13:29 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,55 +1,11 @@
-import React, {
- useState,
- useEffect,
- useCallback,
- useContext,
- useReducer,
- createContext,
-} from 'react'
+import React from 'react'
 import './App.css'
-import Login from './components/Login'
-import Header from './components/Header'
-import CreatePost from './components/CreatePost'
-import PostList from './components/PostList'
-import postReducer from './reducer/postReducer'
-
-export const UserContext = createContext()
-export const PostContext = createContext({
- posts: [],
-})
 
 function App() {
- const initialPostState = useContext(PostContext)
- const [state, dispatch] = useReducer(postReducer, initialPostState)
- const [user, setUser] = useState('reed')
-
- useEffect(() => {
-  document.title = user ? `${user}'s  Feed` : 'Please Login'
- }, [user])
-
- //przy kazdym renderze tworzymy unikalna kopie naszego callbacka
- //przekazywanego do komponentu , zeby tego unikanc uzywamy useCallback
- // const handleAddPost = useCallback(
- //  (newPost) => {
- //   setPosts([newPost, ...posts])
- //  },
- //  [posts]
- // )
-
- if (!user) {
-  return <Login setUser={setUser} />
- }
  return (
-  <PostContext.Provider value={{state, dispatch}}>
-   <UserContext.Provider value={user}>
-    <Header user={user} setUser={setUser} />
-    <CreatePost
-     user={user}
-     // handleAddPost={handleAddPost}
-    />
-    <PostList posts={state.posts} />
-   </UserContext.Provider>
-  </PostContext.Provider>
+  <div>
+   <h3>App</h3>
+  </div>
  )
 }
 
